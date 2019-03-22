d8ab9a8ebcfc8a19fb0beb3d3352c1ca963efe72
syYing <706580680@qq.com>
2019-03-22 16:05:00 +0800

    Update TankClient.java

--- TankClient.java
+++ TankClient.java
@@ -21,11 +21,11 @@ public class TankClient extends Frame{
         TankClient tc = new TankClient();
         tc.launchFrame();
     }
-    /* Game screen size */
+    /* 游戏屏幕大小 */
     public static final int GAME_WIDTH = 800;
     public static final int GAME_HEIGHT = 600;
 
-    private Tank tk = new Tank(50, 50, true, this);
+    private Tank tk = new Tank(50, 50, true, this); // 主坦克
     private List<Tank> enemyTanks = new ArrayList<>();
     private List<Missile> missiles = new ArrayList<>();
     private List<Explode> explodes = new ArrayList<>();
@@ -34,13 +34,14 @@ public class TankClient extends Frame{
     private Blood b = new Blood();
 
     private Random r = new Random();
-    private Image offScreenImage = null; // Buffered image
+    private Image offScreenImage = null; // 缓存图像
 
     public void launchFrame() {
         this.setLocation(300, 400);
         this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setTitle("TankWar");
         this.setBackground(Color.GRAY);
+        // 为关闭窗口添加响应
         this.addWindowListener(new WindowAdapter() {
 
             @Override
@@ -57,6 +58,10 @@ public class TankClient extends Frame{
 
     @Override
     public void paint(Graphics g) {
+        /**
+         * 画出主坦克
+         * 如果主坦克存活，则画出来；否则提示游戏结束
+         */
         if(tk.isLive()) {
             tk.draw(g);
             tk.hitWall(w1);
@@ -64,17 +69,22 @@ public class TankClient extends Frame{
         } else {
             g.drawString("Game Over, press R to revive!", GAME_WIDTH / 2 - 100, GAME_HEIGHT / 2);
         }
-        
+        /**
+         * 画出敌方坦克
+         * 如果敌方坦克数量为0，则产生随即数量的敌方坦克
+         */
         if(enemyTanks.size() == 0) {
             produceTank();
         }
         for (int i = 0; i < enemyTanks.size(); i++) {
             Tank enemy = enemyTanks.get(i);
-            enemy.acrossWithTanks(enemyTanks);
+            // 坦克之间的体积碰撞
+            enemy.acrossWithTanks(enemyTanks); 
             if(!enemy.isLive()) {
                 enemyTanks.remove(enemy);
             } else {
                 enemy.draw(g);
+                // 坦克与墙之间的体积碰撞
                 enemy.hitWall(w1);
                 enemy.hitWall(w2);
             }
@@ -106,6 +116,7 @@ public class TankClient extends Frame{
         w2.draw(g);
 
         b.draw(g);
+        // 如果主坦克吃掉血块，则重新生成血块
         if(tk.eatBlood(b)) {
             b = new Blood();
         }
@@ -118,6 +129,7 @@ public class TankClient extends Frame{
         if(offScreenImage == null) {
             offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
         }
+        // 重新定义一个画布
         Graphics screen = offScreenImage.getGraphics();
         Color c = screen.getColor();
         screen.setColor(Color.GRAY);
@@ -132,6 +144,7 @@ public class TankClient extends Frame{
         public void run() {
             
             while(true) {
+                // 每50ms重画一次
                 repaint();
                 try {
                     Thread.sleep(50);
@@ -154,6 +167,9 @@ public class TankClient extends Frame{
         }
     }
 
+    /**
+     * 产生敌方坦克
+     */
     public void produceTank() {
         int num = r.nextInt(7) + 3;
 
